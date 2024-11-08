b60795d40dcafff699bc6508f9a8d94f2f453ce7
Quent740 <Quentindg740@gmail.com>
2024-11-08 15:28:56 +0100

    fonctionalite php Add + connect + close (non modifié)

--- config/connect.php
+++ config/connect.php
@@ -1,9 +1,8 @@
 <?php
 try{
-    $db = new PDO('mysql:host=localhost;dbname=cruddb', 'root','');
+    $db = new PDO('mysql:host=localhost;dbname=getstages.sql', 'root','');
     $db->exec('SET NAMES "UTF8"');
     } catch (PDOException $e){
     echo 'Erreur : '. $e->getMessage();
     die();
     }
-?>
