3e54cb64af63883c65aebb1e16ee793d2e9e6548
p2405547 <rexgaming.delaveaud@gmail.com>
2024-11-08 14:51:37 +0100

    les fichiers

---
+++ public/accueil.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="CSS/general.css">
+
+
+</head>
+<body>
+
+</body>
+
+
+</html>
